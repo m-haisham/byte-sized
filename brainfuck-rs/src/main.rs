e2794c06807216766e57ccb3d21e7095e0640ed5
Mensch <mensch272@users.noreply.github.com>
2022-07-02 11:25:31 +0500

    feat(brainfuck-rs): added commandline.

--- brainfuck-rs/src/main.rs
+++ brainfuck-rs/src/main.rs
@@ -1,9 +1,27 @@
 mod lib;
 
+use std::{fs, process::exit};
+
 use lib::BrainFuck;
 
 fn main() {
-    let program = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+    let args = std::env::args().collect::<Vec<_>>();
+    let program = match args.len() {
+        2 => args[1].to_owned(),
+        3 => {
+            if args[1] == "-f" {
+            } else {
+                panic!("Expected file path '-f'");
+            }
+
+            fs::read_to_string(args[2].clone()).unwrap()
+        }
+        _ => {
+            println!("Usage: brainfuck-rs (<program> | [-f] <path>)");
+            exit(2);
+        }
+    };
+
     let mut compiler = BrainFuck::new();
-    compiler.compile(program);
+    compiler.compile(program.as_str());
 }
